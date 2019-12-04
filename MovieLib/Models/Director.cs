a089caf1521462707a35ea314788b33ca3755ff5
MJBackes <mjohnbackes@gmail.com>
2019-12-04 10:09:12 -0600

    Added models for Movie and Director.

---
+++ MovieLib/Models/Director.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieLib.Models
+{
+    public class Director
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastNme { get; set; }
+    }
+}
---
+++ MovieLib/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MovieLib.Models
+{
+    public class Movie
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Runtime { get; set; }
+        [ForeignKey("Director")]
+        public Guid DirectorId { get; set; }
+        public Director Director { get; set; }
+    }
+}
